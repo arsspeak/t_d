4b94d4b84fdc21b783e85b621da5d4b0390e7018
arsspeak0000@gmail.com <arsspeak0000@gmail.com>
2019-12-08 15:51:25 +0700

    init

--- src/Map/Map.java
+++ src/Map/Map.java
@@ -3,6 +3,8 @@ package Map;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+import java.util.Arrays;
+
 public class Map {
     Image img = new Image("map.png");
 
@@ -11,5 +13,42 @@ public class Map {
         gc.drawImage(img, 0, 0);
     }
 
-    public boolean[][] Placable = new boolean[10][10]; // Quantize some coordinates then refer to this table
+    private boolean[][] Placable = new boolean[20][15];
+    private boolean[][] Occupied = new boolean[20][15];
+
+
+    public Map()
+    {
+        Arrays.fill(Placable, true);
+        Arrays.fill(Occupied, false);
+        for(int i=0; i<20; i++) Placable[i][2] = false;
+        for(int i=0; i<20; i++) Placable[i][3] = false;
+        for(int i=0; i<20; i++) Placable[i][4] = false;
+        for(int i=0; i<2; i++) Placable[i][5] = false;
+        for(int i=0; i<19; i++) Placable[i][6] = false;
+        for(int i=0; i<19; i++) Placable[i][7] = false;
+        for(int i=0; i<19; i++) Placable[i][8] = false;
+        for(int i=16; i<19; i++) Placable[i][9] = false;
+        for(int i=16; i<19; i++) Placable[i][10] = false;
+        for(int i=0; i<19; i++) Placable[i][11] = false;
+        for(int i=0; i<19; i++) Placable[i][12] = false;
+        for(int i=0; i<19; i++) Placable[i][13] = false;
+    }
+
+    public boolean available(int X, int Y)
+    {
+        if(Placable[X][Y]==true && Occupied[X][Y]==false) return true;
+        else return false;
+    }
+
+    public void occupy(int X, int Y)
+    {
+        Occupied[X][Y]=true;
+    }
+
+    public void release(int X, int Y)
+    {
+        Occupied[X][Y]=false;
+    }
+
 }
